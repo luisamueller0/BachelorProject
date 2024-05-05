8acda5b52f75c4007cd8a53086280f8977cc20e4
luisamueller0 <luisamueller02@web.de>
2024-05-05 15:57:51 +0200

    Worked on adding more information to artists

--- backend/db.py
+++ backend/db.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     print(f"Execution time: {execution_time} seconds.")
 
 """
-# Update the techniques for each CatalogueEntry
+""" # Update the techniques for each CatalogueEntry
 # Some catalogueEntries have missing artforms but techniques exist (905)
 # Use of json "missing_artforms_categories.json" to update the artform for these entries
 file_path = 'missing_artforms_categories.json'
@@ -71,14 +71,14 @@ def update_entriesTechniques(driver):
     print("data read")
     
     with driver.session() as session:
-        session.run("""
+        session.run(
             UNWIND $data AS value
             WITH value.technique AS technique, value.category AS artForm
 
 MATCH (n:CatalogueEntry)
 WHERE n.technique = technique AND n.artForm = 'missing'
 SET n.artForm = artForm
-        """, data=data) 
+        , data=data) 
     print(f"Updated {file_path} successfully.")
 
 def getTotal(driver):
@@ -91,6 +91,56 @@ if __name__ == "__main__":
     start_time = time.time()
     update_entriesTechniques(driver)
    
+    driver.close()  # Close the driver when finished
+    end_time = time.time()
+    execution_time = round( end_time - start_time ) # Calculate the execution time
+    print(f"Execution time: {execution_time} seconds.")
+ """
+# Create GeoName label from official neo4j website to get a way to retrieve the birthcountry from artist birthplaces
+# Use of json "geoName.json" to create these labels
+file_path = 'geoName.json'
+
+# Create a driver instance
+uri = "bolt://localhost:7687"
+username = "neo4j"
+password = "24032102"
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+def load_json_data(file_path):
+    with open(file_path, 'r', encoding='utf-8-sig') as file:  # Specify UTF-8 encoding here
+        data = json.load(file)
+    return data
+
+def create_entries(driver):
+    data = load_json_data(file_path)
+    print("data read")
+    
+    with driver.session() as session:
+        result = session.run("""
+            UNWIND $data AS geoName
+            CREATE (n:GeoName {
+                country: geoName.country,
+                place_alt: geoName.place_alt,
+                latitude: geoName.latitude,
+                tgn: geoName.tgn,
+                place: geoName.place,
+                id: geoName.id,
+                type: geoName.type,
+                longitude: geoName.longitude
+            })
+RETURN count(n) as createdNodes
+        """, data=data)
+        print(f"Nodes created: {result.single()['createdNodes']}")
+    print(f"Updated database with data from {file_path} successfully.")
+
+def getTotal(driver):
+    with driver.session() as session:
+        result = session.run("MATCH (n:CatalogueEntry) RETURN count(n) as total", technique="\\N")
+        print(result.single()['total'])
+
+if __name__ == "__main__":
+    start_time = time.time()
+    create_entries(driver)
     driver.close()  # Close the driver when finished
     end_time = time.time()
     execution_time = round( end_time - start_time ) # Calculate the execution time
