4c77602570e33c3a3b9d92c52cd2c3379607206c
luisamueller0 <luisamueller02@web.de>
2024-05-21 14:48:16 +0200

    Tried to add ulan

--- backend/db.py
+++ backend/db.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     execution_time = round( end_time - start_time ) # Calculate the execution time
     print(f"Execution time: {execution_time} seconds.")
  """
-# Create GeoName label from official neo4j website to get a way to retrieve the birthcountry from artist birthplaces
+""" # Create GeoName label from official neo4j website to get a way to retrieve the birthcountry from artist birthplaces
 # Use of json "geoName.json" to create these labels
 file_path = 'geoName.json'
 
@@ -116,7 +116,7 @@ def create_entries(driver):
     print("data read")
     
     with driver.session() as session:
-        result = session.run("""
+        result = session.run(
             UNWIND $data AS geoName
             CREATE (n:GeoName {
                 country: geoName.country,
@@ -129,7 +129,7 @@ def create_entries(driver):
                 longitude: geoName.longitude
             })
 RETURN count(n) as createdNodes
-        """, data=data)
+        , data=data)
         print(f"Nodes created: {result.single()['createdNodes']}")
     print(f"Updated database with data from {file_path} successfully.")
 
@@ -144,5 +144,48 @@ if __name__ == "__main__":
     driver.close()  # Close the driver when finished
     end_time = time.time()
     execution_time = round( end_time - start_time ) # Calculate the execution time
-    print(f"Execution time: {execution_time} seconds.")
+    print(f"Execution time: {execution_time} seconds.") """
 
+# Create GeoName label from official neo4j website to get a way to retrieve the birthcountry from artist birthplaces
+# Use of json "geoName.json" to create these labels
+file_path = 'artists.json'
+
+# Create a driver instance
+uri = "bolt://localhost:7687"
+username = "neo4j"
+password = "24032102"
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+
+def load_json_data(file_path):
+    with open(file_path, 'r', encoding='utf-8-sig') as file:  # Specifying UTF-8 encoding
+        data = json.load(file)
+    return data
+
+
+def update_entriesTechniques(driver):
+    data = load_json_data(file_path)
+    print("data read")
+    
+    with driver.session() as session:
+        session.run("""
+         UNWIND $data AS value
+            MATCH (n:Artist {id: value.id})
+            SET n.pnd = value.pnd, n.ulan = value.ulan
+        """, data=data) 
+    print(f"Updated {file_path} successfully.")
+
+def getTotal(driver):
+    with driver.session() as session:
+        result = session.run("MATCH (n:Artist) RETURN count(n) as total")
+        print(result.single()['total'])
+
+if __name__ == "__main__":
+    getTotal(driver)
+    start_time = time.time()
+    update_entriesTechniques(driver)
+   
+    driver.close()  # Close the driver when finished
+    end_time = time.time()
+    execution_time = round( end_time - start_time ) # Calculate the execution time
+    print(f"Execution time: {execution_time} seconds.")
