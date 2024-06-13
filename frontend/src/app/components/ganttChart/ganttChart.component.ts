86988200ae4a4917ee1ca9b9ef7e53427398ce86
Luisa <luisamueller02@web.de>
2024-06-13 17:35:27 +0200

    Fixed coloring

--- frontend/src/app/components/ganttChart/ganttChart.component.ts
+++ frontend/src/app/components/ganttChart/ganttChart.component.ts
@@ -121,10 +121,11 @@ export class GanttChartComponent implements OnInit, OnChanges, OnDestroy {
     }
 
     private drawTimeline(): void {
-      const exhibitions :Exhibition[]|null= this.exhibitions;
-      if(exhibitions ===null){
+      const exhibitions: Exhibition[] | null = this.exhibitions;
+      if (exhibitions === null) {
         return;
       }
+    
       const timelineData = exhibitions.map(exhibition => ({
         name: exhibition.name,
         start: new Date(exhibition.start_date).getTime(),
@@ -134,61 +135,58 @@ export class GanttChartComponent implements OnInit, OnChanges, OnDestroy {
         country: exhibition.took_place_in_country,
         normalizedParticipants: 0 // Add this property for initialization
       }));
-
+    
       const normalizedParticipants = this.normalizeLogarithmically(timelineData.map(d => d.amountParticipants));
-
+    
       timelineData.forEach((d, i) => {
         d.normalizedParticipants = normalizedParticipants[i];
       });
-
+    
       const groupedByCountry = d3.group(timelineData, d => d.country);
       const sortedCountries = Array.from(groupedByCountry.entries())
         .sort(([, a], [, b]) => d3.descending(a.length, b.length)) // Sort by number of exhibitions
         .map(([country]) => country);
-
+    
       const xScale = d3.scaleTime()
         .domain([d3.min(timelineData, d => d.start)!, d3.max(timelineData, d => d.end)!])
         .range([0, this.contentWidth])
         .nice();
-
+    
       const yScale = d3.scaleBand()
         .domain(timelineData.map((d, i) => i.toString()))
         .range([0, this.contentHeight])
         .padding(0.1);
-
+    
       const colorScale = d3.scaleSequential(d3.interpolatePlasma)
         .domain([0, 1]); // Normalized participants are between 0 and 1
-
+  
+    
       // Create x-axis with grid lines at the top
       const xAxis = d3.axisTop(xScale).tickSize(-this.contentHeight);
-
+    
       const xAxisGroup = this.svg.append('g')
-      .call(xAxis)
-      .attr('transform', `translate(0,0)`)
-      .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(65)")
-      .style("font-size", "0.5vw");  // Change font size of x-axis labels
+        .call(xAxis)
+        .attr('transform', `translate(0,0)`)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(65)")
+        .style("font-size", "0.5vw");  // Change font size of x-axis labels
     
-    
-  
-
       // Change the color of the grid lines on the x-axis
       this.svg.selectAll('.tick line')
         .attr('stroke', 'gray');  // Change this to the desired color
-
-      const extraSpace = 0.2* window.innerWidth / 100; // 2vw space between countries
-
+    
+      const extraSpace = 0.2 * window.innerWidth / 100; // 2vw space between countries
+    
       let yOffset = 0;
       sortedCountries.forEach((country, index) => {
         const exhibitions = groupedByCountry.get(country)!;
         exhibitions.sort((a, b) => d3.ascending(a.start, b.start));
-
+    
         const countryColor = this.artistService.getCountryColor(country, 0.1); // Lighter background color
-       
-
+    
         // Draw background for the country section
         this.svg.append('rect')
           .attr('x', 0)
@@ -196,7 +194,7 @@ export class GanttChartComponent implements OnInit, OnChanges, OnDestroy {
           .attr('width', this.contentWidth)
           .attr('height', exhibitions.length * yScale.bandwidth())
           .attr('fill', countryColor);
-
+    
         // Add country label
         this.svg.append('text')
           .attr('class', 'label')
@@ -207,85 +205,94 @@ export class GanttChartComponent implements OnInit, OnChanges, OnDestroy {
           .attr('fill', this.artistService.getCountryColor(country))
           .style('font-size', '0.5vw')  // Change font size for country labels
           .text(country);
-
-        // Draw bars for each exhibition
+    
+        // Draw bars or circles for each exhibition
         exhibitions.forEach(exhibition => {
-          this.svg.append('rect')
-            .attr('class', 'bar')
-            .attr('x', xScale(exhibition.start))
-            .attr('y', yOffset)
-            .attr('width', xScale(exhibition.end) - xScale(exhibition.start) === 0 ? 1 : xScale(exhibition.end) - xScale(exhibition.start))
-            .attr('height', yScale.bandwidth())
-            .attr('fill', colorScale(exhibition.normalizedParticipants));
+          const isSingleDay = exhibition.start === exhibition.end;
+    
+          if (isSingleDay) {
+            // Draw a circle
+            this.svg.append('circle')
+              .attr('class', 'circle')
+              .attr('cx', xScale(exhibition.start))
+              .attr('cy', yOffset + yScale.bandwidth() / 2)
+              .attr('r', yScale.bandwidth() / 2) 
+             .attr('fill', timelineData.length !== 1 ? colorScale(exhibition.normalizedParticipants) : colorScale(1));
+          } else {
+            // Draw a bar
+            this.svg.append('rect')
+              .attr('class', 'bar')
+              .attr('x', xScale(exhibition.start))
+              .attr('y', yOffset)
+              .attr('width', xScale(exhibition.end) - xScale(exhibition.start) === 0 ? 1 : xScale(exhibition.end) - xScale(exhibition.start))
+              .attr('height', yScale.bandwidth())
+              .attr('fill', timelineData.length !== 1 ? colorScale(exhibition.normalizedParticipants) : colorScale(1));
+          }
           yOffset += yScale.bandwidth();
         });
-
+    
         yOffset += extraSpace; // Extra space between different country groups
       });
-
-     
-  // Add legend
-  const legendHeight = 10;
-  const legendWidth = 100;
-  const legendX = 80; // Position legend on the top
-  const legendY = -80; // Adjust legend position
-
-  const legend = this.svg.append('g')
-    .attr('transform', `translate(${legendX}, ${legendY})`);
-
-  // Legend title
-
-
-  // Create gradient for legend
-  const defs = this.svg.append('defs');
-  const linearGradient = defs.append('linearGradient')
-    .attr('id', 'linear-gradient')
-    .attr('x1', '0%')
-    .attr('y1', '0%')
-    .attr('x2', '100%')
-    .attr('y2', '0%');
-
-  const stops = d3.range(0, 1.01, 0.01).map((t: number) => ({
-    offset: `${t * 100}%`,
-    color: colorScale(t)
-  }));
-
-  linearGradient.selectAll('stop')
-    .data(stops)
-    .enter().append('stop')
-    .attr('offset', (d: { offset: string; color: string }) => d.offset)
-    .attr('stop-color', (d: { offset: string; color: string }) => d.color);
-
-  // Draw legend
-  legend.append('rect')
-    .attr('width', legendWidth)
-    .attr('height', legendHeight)
-    .style('fill', 'url(#linear-gradient)');
-
-  // Legend axis with normalized values from 0 to 1
-  const legendScale = d3.scaleLinear()
-    .domain([0, 1])
-    .range([0, legendWidth]);
-
-  const legendAxis = d3.axisBottom(legendScale)
-    .ticks(6)
-    .tickFormat(d3.format(".1f")); // Format ticks to show one decimal place
-
-  
-  const legendGroup=legend.append('g')
-    .attr('transform', `translate(0, ${legendHeight})`)
-    .call(legendAxis)
-    legendGroup.selectAll('text')
-    .style('font-size', '0.5vw'); // Smaller font size for legend axi
-// Add participants label
-legend.append('text')
-.attr('x', -10)
-.attr('y', legendHeight + 25) // Adjust this to position the label correctly
-.attr('text-anchor', 'start')
-.style('font-size', '0.5vw')
-.text('Normalized Number of Participants');
     
+      // Add legend
+      const legendHeight = 10;
+      const legendWidth = 100;
+      const legendX = 80; // Position legend on the top
+      const legendY = -80; // Adjust legend position
+    
+      const legend = this.svg.append('g')
+        .attr('transform', `translate(${legendX}, ${legendY})`);
+    
+      // Create gradient for legend
+      const defs = this.svg.append('defs');
+      const linearGradient = defs.append('linearGradient')
+        .attr('id', 'linear-gradient')
+        .attr('x1', '0%')
+        .attr('y1', '0%')
+        .attr('x2', '100%')
+        .attr('y2', '0%');
+    
+      const stops = d3.range(0, 1.01, 0.01).map((t: number) => ({
+        offset: `${t * 100}%`,
+        color: colorScale(t)
+      }));
+    
+      linearGradient.selectAll('stop')
+        .data(stops)
+        .enter().append('stop')
+        .attr('offset', (d: { offset: string; color: string }) => d.offset)
+        .attr('stop-color', (d: { offset: string; color: string }) => d.color);
+    
+      // Draw legend
+      legend.append('rect')
+        .attr('width', legendWidth)
+        .attr('height', legendHeight)
+        .style('fill', 'url(#linear-gradient)');
+    
+      // Legend axis with normalized values from 0 to 1
+      const legendScale = d3.scaleLinear()
+        .domain([0, 1])
+        .range([0, legendWidth]);
+    
+      const legendAxis = d3.axisBottom(legendScale)
+        .ticks(6)
+        .tickFormat(d3.format(".1f")); // Format ticks to show one decimal place
+    
+      const legendGroup = legend.append('g')
+        .attr('transform', `translate(0, ${legendHeight})`)
+        .call(legendAxis)
+      legendGroup.selectAll('text')
+        .style('font-size', '0.5vw'); // Smaller font size for legend axis
+    
+      // Add participants label
+      legend.append('text')
+        .attr('x', -10)
+        .attr('y', legendHeight + 25) // Adjust this to position the label correctly
+        .attr('text-anchor', 'start')
+        .style('font-size', '0.5vw')
+        .text('Normalized Number of Participants');
     }
+    
     
 
     private normalizeLogarithmically(values: number[]): number[] {
