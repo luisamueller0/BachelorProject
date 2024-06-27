0b487b0b67a47922d113c392f7f1814edccdd24f
Luisa <luisamueller02@web.de>
2024-06-27 22:24:25 +0200

    Cleaned up frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,34 +4,24 @@ import { HttpClientModule } from '@angular/common/http';  // Import HttpClientMo
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VisualizationComponent } from './components/visualization/visualization.component';
 import { DecisionsComponent } from './components/decisions/decisions.component';
 import { ArtistService } from './services/artist.service';
 import { BarchartComponent } from './components/barchart/barchart.component';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { ClusterVisualizationComponent } from './components/clusterVisualization/clusterVisualization.component';
 import { MapComponent } from './components/map/map.component';
-import { FocusOnClusterComponent } from './components/focusOnCluster/focusOnCluster.component';
-import { ExhibitionVisualizationComponent } from './components/exhibitionVisualization/exhibitionVisualization.component';
 import { SmallMultiplesComponent } from './components/smallMultiples/smallMultiples.component';
 import { ExhibitionBarchartComponent } from './components/exhibitionBarchart/exhibitionBarchart.component';
-import { PlotComponent } from './components/plot/plot.component';
 import { GanttChartComponent } from './components/ganttChart/ganttChart.component';
 import { ArtistGanttChartComponent } from './components/artistGanttChart/artistGanttChart.component';
 @NgModule({
   declarations: [
     AppComponent,
-    VisualizationComponent,
     DecisionsComponent,
     BarchartComponent,
-    ClusterVisualizationComponent,
     MapComponent,
-    FocusOnClusterComponent,
-    ExhibitionVisualizationComponent,
     SmallMultiplesComponent,
     ExhibitionBarchartComponent,
-    PlotComponent,
     GanttChartComponent,
     ArtistGanttChartComponent
   ],
