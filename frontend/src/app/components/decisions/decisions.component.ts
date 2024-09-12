b9eef7c738ac87114c1104a4873c49a51df368cb
Luisa <luisamueller02@web.de>
2024-09-12 15:44:31 +0200

    changed max cluster numbers2

--- frontend/src/app/components/decisions/decisions.component.ts
+++ frontend/src/app/components/decisions/decisions.component.ts
@@ -88,7 +88,7 @@ public isLoadingRange: boolean = false;
   k: number = 7; // Initial K value
   kOptions: Options = {
     floor: 1,
-    ceil: 7,
+    ceil: 11,
     step: 1,
   };
   private subscription: Subscription = new Subscription();
@@ -142,7 +142,7 @@ public isLoadingRange: boolean = false;
       this.numberOfArtists = this.artists.length;
 
       // Step 2: Update kOptions based on the fetched artists
-      const newCeil = Math.ceil(this.artists.length / 15);
+      const newCeil = Math.ceil(this.artists.length / 10);
       if (newCeil < 1 && this.artists.length > 0) {
         this.kOptions = { ...this.kOptions, ceil: 1 }; // Set ceil to 1 if calculation results in less than 1
       } else {
